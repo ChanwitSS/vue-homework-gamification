f94329ec15df8357aa138259e8d85525f4506ca5
ChanwitSS <chanwit.sa@ku.th>
2021-08-08 02:24:09 +0700

    reslove conflict (package)

--- src/views/Leaderboard.vue
+++ src/views/Leaderboard.vue
@@ -5,12 +5,7 @@
 </template>
 
 <script>
-<<<<<<< HEAD
 import BoardTable from "@/components/board/BoardTable.vue";
-=======
-import BoardTable from "@/components/board/boardTable.vue";
-import Session from "@/components/Session.vue";
->>>>>>> origin/Jerry
 export default {
   components: { BoardTable, },
   data() {
